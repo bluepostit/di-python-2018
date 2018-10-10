beb2b0439ec161970762c05f72e26436c2166123
Yair G <bluepostit101@gmail.com>
2018-10-10 09:14:35 +0300

    Answer for day 1, ex. 1

---
+++ week1/day1-exercise1.py
@@ -0,0 +1,9 @@
+# Exercise 1
+name = input('What is your name? ')
+age = input('How old are you? ')
+shoe_size = input('What is your shoe size? ')
+info = 'Your name is ' + name + ', you are ' + str(age) \
+    + ', your shoe size is ' + str(shoe_size) \
+    + ', and you like vanilla ice cream.'
+
+print(info)
