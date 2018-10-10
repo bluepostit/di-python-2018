dc6754a2ede1ca1cdbdae4edcd131d4e0a871ccb
Yair G <bluepostit101@gmail.com>
2018-10-10 09:10:20 +0300

    Add group exercise answer for day 1

---
+++ week1/day1-group-exercise.py
@@ -0,0 +1,10 @@
+# Simple Greeter
+
+name = 'Yair'
+age = 31
+shoe_size = 11
+info = 'My name is ' + name + ', I am ' + str(age) \
+    + ', my shoe size is ' + str(shoe_size) \
+    + ', and I like vanilla ice cream.'
+
+print(info)
